b810f3541654b524dbe0191deabdaacfc6db8c3f
Robert Sliwinski <robert.sliwinski@inbox.com>
2022-06-01 11:49:32 +0100

    files name update

--- src/redux-toolkit/store.ts
+++ src/redux-toolkit/store.ts
@@ -1,15 +1,14 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import ReduxThunk from 'redux-thunk';
 import { default as tasksSlice } from './slices/tasksSlice';
+
 const pReducer = combineReducers({
     tasks: tasksSlice
 });
 
 export const storeReduxToolkit = configureStore({
     reducer: pReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false
-    }).concat(ReduxThunk)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(ReduxThunk)
 });
 
 export type RootState = ReturnType<typeof storeReduxToolkit.getState>;
